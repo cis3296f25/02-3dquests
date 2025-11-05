c4fef42e7a305d08ccd60a7aa9c0043b7b1b4449
perialquiros <tuq12091@temple.edu>
2025-11-05 15:25:55 -0500

    created new node to store objects
    $Node3D in camera test to help with saving and loading

--- godot/3d-quests/scripts/camera_test.gd
+++ godot/3d-quests/scripts/camera_test.gd
@@ -2,6 +2,7 @@ extends Node3D
 
 @onready var camera: Camera3D = $PlacerCamera
 @onready var object: Node3D = $GhostObject
+
 var min_pos: Vector3 = Vector3(-75, 0, -75)
 var max_pos: Vector3 = Vector3(75, 10, 75)
 func _ready() -> void:
@@ -17,6 +18,6 @@ func _process(delta: float) -> void:
 func place_object():
 	var props = object.get_properties()
 	var new_obj: StaticBody3D = props["mesh"].instantiate()
-	add_child(new_obj)
+	$Node3D.add_child(new_obj)
 	new_obj.global_position = props["position"]
 	new_obj.global_rotation = props["rotation"]
