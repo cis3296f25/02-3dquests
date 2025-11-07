9641781204c848cf2ec6937d3c3400cae5031736
s0dl <tui91919@temple.edu>
2025-11-07 10:23:02 -0500

    Added actual functionality to the menu

--- godot/3d-quests/scenes/object_menu.gd
+++ godot/3d-quests/scenes/object_menu.gd
@@ -1,6 +1,7 @@
 extends Control
 
 @onready var modal_window = $ObjectMenuWindow
+@onready var grid = $ObjectMenuWindow/PanelContainer/ScrollContainer/GridContainer
 
 signal object_selected(scene: PackedScene)
 
@@ -8,26 +9,40 @@ func open_inventory():
 	modal_window.popup_centered()
 
 func add_asset_buttons(asset_pack: Array[PackedScene]):
-	var grid = modal_window/ScrollContainer/GridContainer
 	
 	for child in grid.get_children():
 		child.queue_free()
 	
 	for asset in asset_pack:
 		var btn = Button.new()
+		btn.custom_minimum_size = Vector2(80, 80)
 		btn.text = asset.resource_path.get_file().get_basename()
 		btn.pressed.connect(Callable(self, "_on_asset_pressed").bind(asset))
 		grid.add_child(btn)
+	
 
 func _on_asset_pressed(asset):
 	emit_signal("object_selected", asset)
 	modal_window.hide()
 
+var asset_categorizer: AssetCategorizer
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	#modal_window.close_requested(modal_window.hide)
-	pass
+	modal_window.close_requested.connect(_on_modal_closed)
+	asset_categorizer = AssetCategorizer.new()
+	asset_categorizer.scan_and_categorize("res://")
+	
+	
+	var wall_assets = asset_categorizer.get_assets_by_category("Wall")
+	var scenes: Array[PackedScene] = []
+	for path in wall_assets:
+		var scene = load(path)
+		if scene:
+			scenes.append(scene)
 
+	add_asset_buttons(scenes)
+	
 func _on_modal_closed():
 	modal_window.hide()
 
--- godot/3d-quests/scripts/camera_test.gd
+++ godot/3d-quests/scripts/camera_test.gd
@@ -3,6 +3,7 @@ extends Node3D
 @onready var camera: Camera3D = $PlacerCamera
 @onready var object: Node3D = $GhostObject
 @onready var main_ui = $UILayer/MainUIControl
+@onready var object_menu = $UILayer/ObjectMenuControl
 
 var can_place := false
 var selected_object: Node3D = null
@@ -15,12 +16,17 @@ func _ready() -> void:
 	main_ui.place_mode_changed.connect(_on_mode_changed)
 	if object:
 		object.visible = can_place
+	object_menu.object_selected.connect(_on_object_changed)
 	
 func _on_mode_changed(enabled: bool) -> void:
 	can_place = enabled
 	print("Placement mode is now: ", enabled)
 	if object:
 		object.visible = enabled
+		
+func _on_object_changed(scene: PackedScene):
+	object.set_mesh(scene)
+
 
 #NOT FINAL SOLUTION
 func _process(delta: float) -> void:
@@ -31,7 +37,7 @@ func _process(delta: float) -> void:
 #gets info from ghost obj, adds child
 func place_object():
 	var props = object.get_properties()
-	var new_obj: StaticBody3D = props["mesh"].instantiate()
+	var new_obj: Node3D = props["mesh"].instantiate()
 	$Node3D.add_child(new_obj)
 	new_obj.global_position = props["position"]
 	new_obj.global_rotation = props["rotation"]
