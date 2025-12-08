9129b081ddd4ba31da1c003f4b2831c95ebc9e53
perialquiros <tuq12091@temple.edu>
2025-12-08 02:39:41 -0500

    Update route.ts
    changes that were meant for the initial commit

--- src/app/api/[campaign_id]/[map_name]/save/route.ts
+++ src/app/api/[campaign_id]/[map_name]/save/route.ts
@@ -1,25 +1,81 @@
 import { prisma } from "@/lib/prisma";
+import { toJSONSchema } from "better-auth";
 import { NextResponse } from "next/server";
 
-export async function POST(req: Request) {
-  try {
-    const { campaignId, mapData } = await req.json();
+// export async function POST(req: Request) {
+//   try {
+//     const { campaignId, name, mapData } = await req.json();
 
-    const newMap = await prisma.campaignMap.create({
-      data: {
-        campaignId,
-        data: mapData,
-      },
-    });
+//     const map = await prisma.campaignMap.create({
+//       name: name,
+//       data: {
+//         campaignId,
+//         name,
+//         data: mapData,
+//       },
+//     });
 
-    return NextResponse.json({ newMap, saved: true });
+//     return NextResponse.json({ map, saved: true });
   
-} catch (error: any) {
-    console.error("Error saving map: ", error);
+// } catch (error: any) {
+//     console.error("Error saving map: ", error);
 
-    return NextResponse.json(
-      { error: "Failed to save map." },
-      { status: 500 }
-    );
+//     return NextResponse.json(
+//       { error: "Failed to save map." },
+//       { status: 500 }
+//     );
+//   }
+// }
+
+interface Params {
+  campaign_id: string;
+  map_name: string
+}
+
+export async function POST(req: Request, context: {params: Params}){
+  try {
+  const {campaign_id, map_name} = context.params;
+
+  // Reading the JSON 
+  const body = await req.json();
+  const id = body.id;
+  const obj_data = {
+    pos: body.po_arr,
+    rot: body.r_arr,
+    path: body.pa_arr
   }
+
+  let map_save; 
+
+  if (id == -1) {  // New map save
+    map_save = await prisma.campaignMap.create({
+      data: {
+        name: map_name,
+        data: obj_data,
+        campaignId: campaign_id,
+      }
+    })
+  }
+
+  // Overwrite existing map
+  else {
+  map_save = await prisma.campaignMap.update({
+    where: {
+      id: id
+    },
+    data: {
+      name: map_name,
+      data: obj_data
+    }
+  })}  
+  // Return either new or given map ID
+  return NextResponse.json({id: map_save.id, saved: true}, {status: 200})
+
+} catch (error){
+  console.error(error);
+  return NextResponse.json(
+    {error: "Failed to save map"},
+    {status: 500}
+  )
+}
 }
