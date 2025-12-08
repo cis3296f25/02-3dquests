93277e0682edf9e85e405ec5293c34f6240ba8ea
perialquiros <tuq12091@temple.edu>
2025-12-08 09:01:13 -0500

    Update route.ts

--- src/app/api/[campaign_id]/[map_name]/load/route.ts
+++ src/app/api/[campaign_id]/[map_name]/load/route.ts
@@ -2,28 +2,34 @@ import { prisma } from "@/lib/prisma";
 import { success } from "better-auth";
 import { NextResponse } from "next/server";
 
+interface Params {
+    campaign_id: string;
+    map_name: string;
+}
 
 export async function POST(req: Request, context: {params: Params}){
     const {campaign_id, map_name} = context.params;
 
-        if (!map) {
-            return NextResponse.json({
-                success: false,
-                error: "Map not found"
-            },
-                { status: 404 }
-            );
+    const map = await prisma.campaignMap.findFirst({
+        where: {
+            campaignId: campaign_id,
+            name: map_name
         }
+    })
 
+    if (!map) {
         return NextResponse.json({
-            success: true,
-            map_data: map.data
+            success: false,
+            error: "Map not found"
         },
-            { status: 200 }
-        )
-    } catch (error: any) {
-        console.error(error);
+        {status: 404}
+    );
     }
 
-
+    return NextResponse.json({
+        success: true,
+        map_data: map.data
+    },
+    {status: 200}
+    )
 }
